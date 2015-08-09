49a43cb42a71404d48696c0722aafc4a0eeee704
Zorg <standardimo@gmail.com>
2015-08-08 21:16:10 -0400

    Added a new fact

--- app/src/main/java/com/example/android/funfacts/FactBook.java
+++ app/src/main/java/com/example/android/funfacts/FactBook.java
@@ -18,7 +18,8 @@ public class FactBook {
             "The state of Florida is bigger than England",
             "Some penguins can leap 2-3 meters out of the water.",
             "On average, it takes 66 days to form a new habit",
-            "Mammoths still walked the earth when the Great Pyramid was being built"
+            "Mammoths still walked the earth when the Great Pyramid was being built",
+            "Iced tea is the only beverage early humans consumed."
     };
     // Method (abilites: things the object can do)
     public String getFact() {
